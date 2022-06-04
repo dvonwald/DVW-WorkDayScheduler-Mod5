1871c8d428bc9467c3c81694caf0f7ad2cc79038
Dylan <dylan.vonwald@gmail.com>
2022-06-04 02:28:15 -0600

    got the jquery selector to work with the correct ID when clicking save buttons, made other minor changes also that I can't remember, troubleshot why it wasn't working, was missing $ in some places

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 var today = moment();
-var jumboDate = ('#currentDay')
+var jumboDate = $('#currentDay')
 var currentHour = today.format('H');
-var hourNine = ('#hour-9')
-var hourTen = ('#hour-10')
-var hourEleven = ('#hour-11')
-var hourTwelve = ('#hour-12')
-var hourThirteen = ('#hour-13')
-var hourFourteen = ('#hour-14')
-var hourFifteen = ('#hour-15')
-var hourSixteen = ('#hour-16')
-var hourSeventeen = ('#hour-17')
+var hourNine = $('#hour-9')
+var hourTen = $('#hour-10')
+var hourEleven = $('#hour-11')
+var hourTwelve = $('#hour-12')
+var hourThirteen = $('#hour-13')
+var hourFourteen = $('#hour-14')
+var hourFifteen = $('#hour-15')
+var hourSixteen = $('#hour-16')
+var hourSeventeen = $('#hour-17')
 var timeBlocks = $(".time-block")
 // var = getElementByClassName('')
 
 $(jumboDate).text(today.format('MMMM, Do YYYY, h:mm a'));
 
-$(hourNine).on('click', saveHourText)
+// $(hourNine).on('click', saveHourText)
 
 console.log(currentHour)
+console.log(hourNine)
 // console.log($(".time-block").attr('id'))
 console.log(hourNine < currentHour)
 console.log(hourNine == currentHour)
@@ -29,25 +30,39 @@ console.log(hourNine > currentHour)
 
 colorBlock()
 
+
 function colorBlock() {
+    
+    // for (let i = 0; i < hourArray.length; i++) {
+    //     const hourObj = hourArray[i];
+    
+    // }
+    hourArray = $(timeBlocks).attr('id')
+    console.log(hourArray)
+    $(timeBlocks).removeClass('past', 'present', 'future')
+
 if (hourNine < currentHour) {
-    $(timeBlocks).removeClass('present');
-    $(timeBlocks).removeClass('future');
+    // $(timeBlocks).removeClass('present');
+    // $(timeBlocks).removeClass('future');
     $(timeBlocks).addClass('past');
 
 } else if (hourNine == currentHour) {
-    $(timeBlocks).removeClass('future');
-    $(timeBlocks).removeClass('past');
+    // $(timeBlocks).removeClass('future');
+    // $(timeBlocks).removeClass('past');
     $(timeBlocks).addClass('present')
 
 } else if (hourNine > currentHour) {
-    $(timeBlocks).removeClass('present');
-    $(timeBlocks).removeClass('past');
+    // $(timeBlocks).removeClass('present');
+    // $(timeBlocks).removeClass('past');
     $(timeBlocks).addClass('future');
 }
 }
+console.log(hourNine.parentNode)
 
+$(".button").on('click', function() {
+    alert($(this).parent().attr('id'));
+});
 
-function saveHourText() {
-    console.log("Hour 9 Clicked")
-}
+// function saveHourText() {
+    
+// }
