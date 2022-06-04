2a92fd8f072df7128ee9eb95b1a583da1ebdc8fc
Dylan <dylan.vonwald@gmail.com>
2022-06-03 23:03:55 -0600

    Got the colors working sort of, created variables for all IDs, figured out 24h format with momentjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,38 @@
 var today = moment();
 var jumboDate = ('#currentDay')
+var currentHour = today.format('H');
+var hourNine = ('#hour-9')
+var hourTen = ('#hour-10')
+var hourEleven = ('#hour-11')
+var hourTwelve = ('#hour-12')
+var hourThirteen = ('#hour-13')
+var hourFourteen = ('#hour-14')
+var hourFifteen = ('#hour-15')
+var hourSixteen = ('#hour-16')
+var hourSeventeen = ('#hour-17')
+var timeBlocks = $(".time-block")
+// var = getElementByClassName('')
 
 $(jumboDate).text(today.format('MMMM, Do YYYY, h:mm a'));
 
+$(hourNine).on('click', saveHourText)
+
+console.log(currentHour)
+console.log($(".time-block").attr('id'))
+
+// $(timeBlocks).addClass('present')
+colorBlock()
+
+function colorBlock() {
+if (hourNine < currentHour) {
+    $(timeBlocks).addClass('past');
+} else if (hourNine == currentHour) {
+    $(timeBlocks).addclass('present')
+} else (hourNine > currentHour) 
+    $(timeBlocks).addClass('future')
+}
+
+
+function saveHourText() {
+    console.log("Hour 9 Clicked")
+}
