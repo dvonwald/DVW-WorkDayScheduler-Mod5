0fe9db435cc44b0f738ef8def5ca61be65c1408f
Dylan <dylan.vonwald@gmail.com>
2022-06-04 16:57:17 -0600

    cleaned up the javascript file before moving on to the save string to localstorage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-var today = moment();
-var jumboDate = $('#currentDay')
-var currentHour = today.format('H');
-var hourNine = $('#hour-9')
+var today = moment(); // Putting moment.js into a varaible
+var jumboDate = $('#currentDay') // This is the ID for the jumbotron
+var currentHour = today.format('H'); // Using the current time from moment.js to compare to time blocks
+var hourNine = $('#hour-09') // IDs for all time blocks to be able to select and manipulate them, these all return OBJECTS
 var hourTen = $('#hour-10')
 var hourEleven = $('#hour-11')
 var hourTwelve = $('#hour-12')
@@ -10,38 +10,28 @@ var hourFourteen = $('#hour-14')
 var hourFifteen = $('#hour-15')
 var hourSixteen = $('#hour-16')
 var hourSeventeen = $('#hour-17')
-var timeBlocks = $(".time-block")
-// var = getElementByClassName('')
+var timeBlocks = $(".time-block") // selecting all rows that have time-block class
 
-$(jumboDate).text(today.format('MMMM, Do YYYY, h:mm a'));
+$(jumboDate).text(today.format('MMMM, Do YYYY, h:mm a')); // insert the current date/time to jumbotron
 
 // $(hourNine).on('click', saveHourText)
 
-console.log(currentHour)
-console.log(hourNine)
+//Console logs I used to help me troubleshoot and traverse DOM
+// console.log(hourNine)
 // console.log($(".time-block").attr('id'))
+console.log(currentHour)
 console.log(hourNine < currentHour)
 console.log(hourNine == currentHour)
 console.log(hourNine > currentHour)
 // console.log(timeBlocks[1].id.slice(5))
-
 // console.log(typeof timeBlocks)
 
-// $(timeBlocks).addClass('.past')
+colorBlock() // Calling the colorBlock function
 
-colorBlock()
-
-// hourArray = $(timeBlocks).attr('id')
-// console.log(hourArray)
-
-function colorBlock() {
-    $(timeBlocks).removeClass('past', 'present', 'future')
-    for (let i = 0; i < timeBlocks.length; i++) {
-        // console.log(timeBlocks[i].id.slice(5))
-        // var timeBlocksArray = timeBlocks[i].id.slice(5)
-        // console.log(timeBlocksArray)
-        // console.log(currentHour)
-        // timeBlocks[i]
+function colorBlock() { // Delcaring/defining the colorBlock function 
+    $(timeBlocks).removeClass('past', 'present', 'future') // removing classes from all timeBlocks
+    for (let i = 0; i < timeBlocks.length; i++) { // using for loop on the timeBlocks object which is a collection of all time-block objects
+        // console.log(timeBlocks[i])
         if (timeBlocks[i].id.slice(5) < currentHour) {
             $(timeBlocks[i]).removeClass('present');
             $(timeBlocks[i]).removeClass('future');
@@ -51,7 +41,7 @@ function colorBlock() {
             $(timeBlocks[i]).removeClass('past');
             $(timeBlocks[i]).addClass('present');
         } else if (timeBlocks[i].id.slice(5) > currentHour) {
-            console.log(timeBlocks[i].id.slice(5))
+            // console.log(timeBlocks[i].id.slice(5))
             // console.log(this)
             $(timeBlocks[i]).removeClass('present');
             $(timeBlocks[i]).removeClass('past');
